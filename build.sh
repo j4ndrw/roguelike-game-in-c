a256569e72bc77ee5b43740255cb7e502224ce89
j4ndrw <murjan_andreialin@yahoo.com>
2024-07-13 01:56:22 +0300

    feat: we got text boiiii

--- build.sh
+++ build.sh
@@ -2,11 +2,26 @@
 
 set -xe
 
-CFLAGS=""
+
+DEFAULT_TARGET="linux"
+TARGET="$1"
+
+function get_target()
+{
+    if [[ "$TARGET" == "" ]]; then
+        echo $DEFAULT_TARGET
+    else
+        echo $TARGET
+    fi
+}
+
+RAYLIB_FLAGS_INCLUDE="-I./vendor/raylib-5.0_$(get_target)/include"
+RAYLIB_FLAGS_LINKER="-Wl,-rpath=./vendor/raylib-5.0_$(get_target)/lib -L./vendor/raylib-5.0_$(get_target)/lib -lraylib"
+CFLAGS="-Wall -Wextra $RAYLIB_FLAGS_INCLUDE $RAYLIB_FLAGS_LINKER"
 
 mkdir -p target
 mkdir -p target/debug
 mkdir -p target/release
 
-cc $CFLAGS -ggdb -o ./target/debug/game main.c
-cc $CFLAGS -o ./target/release/game main.c
+cc main.c $CFLAGS -ggdb -o ./target/debug/game
+cc main.c $CFLAGS -o ./target/release/game
--- main.c
+++ main.c
@@ -1,7 +1,16 @@
-#include <stdio.h>
+#include "raylib.h"
 
 int main()
 {
-    printf("Hello, world!");
+    InitWindow(1920, 1080, "Roguelike Game");
+    while(!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            const char* text = "Hello, World";
+            int fontSize = 120;
+            const int textSize = MeasureText(text, fontSize);
+            DrawText(text, GetScreenWidth() / 2 - (textSize / 2), GetScreenHeight() / 2, fontSize, BLACK);
+        EndDrawing();
+    }
     return 0;
 }
